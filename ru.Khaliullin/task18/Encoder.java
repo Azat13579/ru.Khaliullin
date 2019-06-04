ceb64c696ba298abe632b464d3d18b732fd40c96
Khaliullin Azat <2kai17@rambler.ru>
2019-06-04 16:18:38 +0300

    modTask18

--- ru.Khaliullin/task18/Encoder.java
+++ ru.Khaliullin/task18/Encoder.java
@@ -1,6 +1,8 @@
 package task18;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 import static java.lang.Integer.parseInt;
 import static task18.Codec.*;
 
@@ -12,80 +14,83 @@ public class Encoder {
      * @return
      * @throws IOException
      */
-    public int choiceEncoder() throws IOException {
+    public void choiceEncoder() throws IOException {
 
         System.out.println("Список кодеров: ");
         for (int i = 0; i < codec.length; i++) {
             System.out.println("Кодер № " + (i+1) + " " + codec[i].getNameCod());
         }
-
-        System.out.println("Введите номер выбранного Вами кодера:");
-        BufferedReader bufReader = new BufferedReader(new InputStreamReader(System.in));
-        String s = bufReader.readLine();
-        Integer userChoice = parseInt(s);
-        System.out.println("Вы выбрали кодер № " + userChoice + "!");
-
-        return userChoice;
     }
 
 
-    public void transformation(int userChoiceForLoadAndSave, File allFormatFile, int userChoiceForSave) throws IOException {
+    public void transformation(int userChoiceForLoadAndSave, File allFormatFile,
+                               int userChoiceForSave) throws IOException {
 
-        if(userChoiceForLoadAndSave == 1){
-            String s = null;
-            File utf8File = new File("utf8.txt");
-            try(InputStream is = new FileInputStream(allFormatFile)){
-                byte[] buf = new byte[100];
-                if(is.read(buf) != -1){
-                    s = new String(buf, codec[userChoiceForSave-1].getNameCod());
-                    System.out.println(s);
-                }
+
+        String s = null;
+        File utf8File = new File(codec[userChoiceForLoadAndSave - 1].getNameCod() + ".txt");
+        try (Reader reader = new InputStreamReader(new FileInputStream(allFormatFile),
+                codec[userChoiceForSave - 1].getNameCod());
+             Writer writer = new OutputStreamWriter(new FileOutputStream(utf8File),
+                codec[userChoiceForLoadAndSave - 1].getNameCod())) {
+
+            int c;
+            while ((c = reader.read()) != -1) {
+
+                writer.write(c);
+
             }
 
-            try(OutputStream os = new FileOutputStream(utf8File)){
-                String s1  = s;
-                os.write(s1.getBytes(codec[userChoiceForLoadAndSave-1].getNameCod()));
-            }
-
-
-
-        }else if(userChoiceForLoadAndSave == 2){
-            String s = null;
-            File windows1251File = new File("windows1251.txt");
-            try(InputStream is = new FileInputStream(allFormatFile)){
-                byte[] buf = new byte[100];
-                if(is.read(buf) != -1){
-                    s = new String(buf, codec[userChoiceForSave-1].getNameCod());
-                    System.out.println(s);
-                }
-            }
-
-            try(OutputStream os = new FileOutputStream(windows1251File)){
-                String s1  = s;
-                os.write(s1.getBytes(codec[userChoiceForLoadAndSave-1].getNameCod()));
-            }
         }
-        else if(userChoiceForLoadAndSave == 3){
-            String s = null;
-            File koi8File = new File("koi8.txt");
-            try(InputStream is = new FileInputStream(allFormatFile)){
-                byte[] buf = new byte[100];
-                if(is.read(buf) != -1){
-                    s = new String(buf, codec[userChoiceForSave-1].getNameCod());
-                    System.out.println(s);
-                }
-            }
 
-            try(OutputStream os = new FileOutputStream(koi8File)){
-                String s1  = s;
-                os.write(s1.getBytes(codec[userChoiceForLoadAndSave-1].getNameCod()));
-            }
-
-        } else {
-            System.out.println("Вы ввели номер несуществующего кодера, повторите выбор!");
-            int newUserChoice = choiceEncoder();
-            transformation(newUserChoice, allFormatFile, userChoiceForSave);
-        }
     }
 
 }
+
+
+
+   /* InputStream is = new FileInputStream(allFormatFile);
+    OutputStream os = new FileOutputStream(utf8File)) { // Reader and Writer
+        byte[] buf = new byte[100];
+        if (is.read(buf) != -1) {
+        s = new String(buf, codec[userChoiceForSave - 1].getNameCod());
+        System.out.println(s);
+        }
+        String s1 = s;
+        os.write(s1.getBytes(codec[userChoiceForLoadAndSave - 1].getNameCod()));
+        }*/
+
+/*}else if(userChoiceForLoadAndSave == 2){
+            String s = null;
+            File windows1251File = new File("windows1251");
+            try(InputStream is = new FileInputStream(allFormatFile);
+                OutputStream os = new FileOutputStream(windows1251File)) {
+                byte[] buf = new byte[100];
+                if (is.read(buf) != -1) {
+                    s = new String(buf, codec[userChoiceForSave - 1].getNameCod());
+                    System.out.println(s);
+                }
+                String s1 = s;
+                os.write(s1.getBytes(codec[userChoiceForLoadAndSave - 1].getNameCod()));
+        }}else if(userChoiceForLoadAndSave == 3){
+            String s = null;
+            File koi8File = new File("koi8");
+            try(InputStream is = new FileInputStream(allFormatFile);
+                OutputStream os = new FileOutputStream(koi8File)){
+                byte[] buf = new byte[100];
+                if(is.read(buf) != -1){
+                    s = new String(buf, codec[userChoiceForSave-1].getNameCod());
+                    System.out.println(s);
+                }
+                String s1  = s;
+                os.write(s1.getBytes(codec[userChoiceForLoadAndSave-1].getNameCod()));
+            }} else {
+            System.out.println("Вы ввели номер несуществующего кодера, повторите выбор!");
+            choiceEncoder();
+            System.out.println("Введите номер выбранного Вами кодера:");
+            BufferedReader bufReader = new BufferedReader(new InputStreamReader(System.in));
+            String s2 = bufReader.readLine();
+            Integer newUserChoice  = parseInt(s2);
+            System.out.println("Вы выбрали кодер № " + newUserChoice + "!");
+            transformation(newUserChoice, allFormatFile, userChoiceForSave);
+        }*/
--- ru.Khaliullin/task18/Use.java
+++ ru.Khaliullin/task18/Use.java
@@ -2,6 +2,8 @@ package task18;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import static java.lang.Integer.parseInt;
 import static task18.Codec.*;
 
 public class Use {
@@ -9,22 +11,48 @@ public class Use {
         Encoder encoder = new Encoder();
 
         Codec[] codec = new Codec[]{UTF8, WINDOWS1251, KOI8};
-        int userChoiceForSave = encoder.choiceEncoder();
+
+        encoder.choiceEncoder();
+        int userChoiceForSave = userСhoice();
+        System.out.println("Вы выбрали кодер № " + userChoiceForSave  + "!");
 
         File allFormatFile = new File("allFormatFile.txt");
         try(OutputStream os = new FileOutputStream(allFormatFile)){
             System.out.println("Введите текст который вы хотите сохранить в файле: ");
             BufferedReader bufReader = new BufferedReader(new InputStreamReader(System.in));
-            String s = bufReader.readLine();
-            os.write(s.getBytes(codec[userChoiceForSave-1].getNameCod()));
+            String s1 = bufReader.readLine();
+            os.write(s1.getBytes(codec[userChoiceForSave-1].getNameCod()));
         }
 
         Path path = allFormatFile.toPath();
         System.out.println(Files.size(path));
 
-        int userChoiceForLoadAndSave = encoder.choiceEncoder();
-        System.out.println(userChoiceForLoadAndSave);
+        encoder.choiceEncoder();
+        int userChoiceForLoadAndSave  = userСhoice();
+        System.out.println("Вы выбрали кодер № " + userChoiceForLoadAndSave   + "!");
 
         encoder.transformation(userChoiceForLoadAndSave, allFormatFile, userChoiceForSave);
     }
+
+
+
+    public static int userСhoice() throws IOException {
+        System.out.println("Введите номер выбранного Вами кодера:");
+        BufferedReader bufReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufReader.readLine();
+        int c  = parseInt(s);
+        if (c<4 && c>0){
+            c = c;
+        }else{
+            System.out.println("Вы ввели номер несуществующего кодера, повторите выбор!");
+            Encoder encoder = new Encoder();
+            encoder.choiceEncoder();
+            int newUserChoice1  = userСhoice();
+            c = newUserChoice1;
+        }
+
+        return c;
+
+    }
+
 }
